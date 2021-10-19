931b0549e1fe72be0eb857a08b115f7946ace6a4
NAWALALSOBAIHHI <nawalsobaihi@gmail.com>
2021-10-19 14:48:59 +0300

    Consolidate Conditional Expression

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -23,51 +23,34 @@ class Item {
           item.name == "Backstage passes to a TAFKAL80ETC concert";
         const isQualityBiggerThan0 = item.quality > 0;
         const isQualityLessThan50 = item.quality < 50;
-      
         const tenDaysOrLessToSell = item.sellIn <= 10;
         const fiveDaysOrLessToSell = item.sellIn <= 5;
         const areNoMoreDaysToSell = item.sellIn < 0;
+        const isNormalItem = !isAgedBrie && !isBackstagePasses && !isSulfuras;
   
-        if (!isAgedBrie && !isBackstagePasses) {
+        if (isNormalItem) {
           if (isQualityBiggerThan0) {
-            if (!isSulfuras) {
+            item.quality--;
+  
+            if (areNoMoreDaysToSell) {
               item.quality--;
             }
           }
-        } else {
-          if (isQualityLessThan50) {
+        } else if (isBackstagePasses) {
+          item.quality++;
+          if (tenDaysOrLessToSell) {
             item.quality++;
-            if (isBackstagePasses) {
-              if (tenDaysOrLessToSell) {
-                if (isQualityLessThan50) {
-                  item.quality++;
-                }
-              }
-              if (fiveDaysOrLessToSell) {
-                if (isQualityLessThan50) {
-                  item.quality++;
-                }
-              }
-            }
           }
-        }
   
-        if (areNoMoreDaysToSell) {
-          if (!isAgedBrie) {
-            if (!isBackstagePasses) {
-              if (isQualityBiggerThan0) {
-                if (!isSulfuras) {
-                  item.quality--;
-                }
-              }
-            } else {
-              item.quality = 0;
-            }
-          } else {
-            if (isQualityBiggerThan0) {
-              item.quality++;
-            }
+          if (fiveDaysOrLessToSell) {
+            item.quality++;
           }
+  
+          if (areNoMoreDaysToSell) {
+            item.quality = 0;
+          }
+        } else if (isAgedBrie && isQualityLessThan50) {
+          item.quality++;
         }
       });
   
